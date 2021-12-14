7b8fea1fc9412664254d24f3249fe7300c5d2925
jackXu <xujinzhang@372163.com>
2021-12-14 15:11:01 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { app,BrowserWindow } = require("electron")
+const path = require("path")
+
+/**
+ * 创建主窗口进程
+ */
+function createWindow () {
+  const win = new BrowserWindow({
+    width:800,
+    height:600,
+    webPreferences: {
+      preload: path.join(__dirname,'mdls','preload.js')
+    }
+  })
+  win.loadFile(path.join(__dirname,'index.html'))
+}
+
+app.whenReady().then(()=>{
+  createWindow()
+
+  app.on("activate",()=>{
+    if (BrowserWindow.getAllWindows().length === 0){
+      createWindow()
+    }
+  })
+})
+
+app.on("window-all-closed",()=>{
+  if (process.platform !== "darwin"){
+    app.quit()
+  }
+})
