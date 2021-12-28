781283a3c25ddb23314297917287961c816b72f6
jackXu <xujinzhang@372163.com>
2021-12-28 18:15:33 +0800

    notification add

--- src/main.js
+++ src/main.js
@@ -1,32 +1,35 @@
-const { app,BrowserWindow } = require("electron")
-const path = require("path")
+const { app, BrowserWindow, Notification } = require("electron");
+const path = require("path");
 
 /**
  * 创建主窗口进程
  */
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
-    width:800,
-    height:600,
+    width: 800,
+    height: 600,
     webPreferences: {
-      preload: path.join(__dirname,'nodes','preload.js')
-    }
-  })
-  win.loadFile(path.join(__dirname,'index.html'))
+      preload: path.join(__dirname, "nodes", "preload.js"),
+    },
+  });
+  win.loadFile(path.join(__dirname, "index.html"));
 }
 
-app.whenReady().then(()=>{
-  createWindow()
+//启动时的消息通知
+function startNotification() {
+  new Notification({ title: "启动通知", body: "启动通知主体" }).show();
+}
 
-  app.on("activate",()=>{
-    if (BrowserWindow.getAllWindows().length === 0){
-      createWindow()
-    }
-  })
-})
+app.whenReady().then(createWindow).then(startNotification);
 
-app.on("window-all-closed",()=>{
-  if (process.platform !== "darwin"){
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
